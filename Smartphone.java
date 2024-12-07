cb3e731e89448afb8f6ca8a249ca7415da0e2e07
Bhavya2200030266 <135822756+Bhavya2200030266@users.noreply.github.com>
2024-12-07 09:15:00 +0530

    Update Smartphone.java

--- Smartphone.java
+++ Smartphone.java
@@ -1,5 +1,34 @@
 package com.klef.jfsd.exam.InheritanceMapping;
 
 public class Smartphone {
+package com.klef.jfsd.exam.InheritanceMapping;
+
+	import javax.persistence.Table;
+
+	@Entity
+	@Table(name = "Smartphone")
+	public class Smartphone extends Device {
+	    private String operatingSystem;
+	    private String cameraResolution;
+
+	    // Getters and Setters
+	    public String getOperatingSystem() {
+	        return operatingSystem;
+	    }
+
+	    public void setOperatingSystem(String operatingSystem) {
+	        this.operatingSystem = operatingSystem;
+	    }
+
+	    public String getCameraResolution() {
+	        return cameraResolution;
+	    }
+
+	    public void setCameraResolution(String cameraResolution) {
+	        this.cameraResolution = cameraResolution;
+	    }
+	}
+
+
 
 }
