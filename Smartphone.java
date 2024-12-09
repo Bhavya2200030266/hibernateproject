6751ad03778b8e6d6d638f8f27a79f99f8f139e4
Bhavya2200030266 <135822756+Bhavya2200030266@users.noreply.github.com>
2024-12-09 19:07:23 +0530

    Smartphone.java

--- Smartphone.java
+++ Smartphone.java
@@ -1,34 +1,49 @@
 package com.klef.jfsd.exam.InheritanceMapping;
 
-public class Smartphone {
-package com.klef.jfsd.exam.InheritanceMapping;
 
-	import javax.persistence.Table;
+import javax.persistence.*;
 
-	@Entity
-	@Table(name = "Smartphone")
-	public class Smartphone extends Device {
-	    private String operatingSystem;
-	    private String cameraResolution;
+@Entity
+@Table(name = "smartphone")
+@PrimaryKeyJoinColumn(name = "smartphone_id")
+public class Smartphone extends Device {
+    @Column(name = "operating_system")
+    private String operatingSystem;
 
-	    // Getters and Setters
-	    public String getOperatingSystem() {
-	        return operatingSystem;
-	    }
+    @Column(name = "camera_resolution")
+    private int cameraResolution;
 
-	    public void setOperatingSystem(String operatingSystem) {
-	        this.operatingSystem = operatingSystem;
-	    }
+    public Smartphone() {}
 
-	    public String getCameraResolution() {
-	        return cameraResolution;
-	    }
+    public Smartphone(String brand, String model, double price, 
+                      String operatingSystem, int cameraResolution) {
+        super(brand, model, price);
+        this.operatingSystem = operatingSystem;
+        this.cameraResolution = cameraResolution;
+    }
 
-	    public void setCameraResolution(String cameraResolution) {
-	        this.cameraResolution = cameraResolution;
-	    }
-	}
+    // Getters and Setters
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
 
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
 
+    public int getCameraResolution() {
+        return cameraResolution;
+    }
 
+    public void setCameraResolution(int cameraResolution) {
+        this.cameraResolution = cameraResolution;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Smartphone{" +
+                "operatingSystem='" + operatingSystem + '\'' +
+                ", cameraResolution=" + cameraResolution +
+                '}';
+    }
 }
